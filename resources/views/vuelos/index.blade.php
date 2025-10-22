527704796e8f2e50341e277a5a12de8186a724e8
Andres <vivasandres2007@gmail.com>
2025-10-22 08:39:20 -0500

    nueva rama

--- resources/views/vuelos/index.blade.php
+++ resources/views/vuelos/index.blade.php
@@ -3,7 +3,43 @@
 
 @section('content')
 
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inicio</title>
+</head>
+<body>
+    <main id="ppal">
+        <figure>
+            <img src="" alt="">
+        </figure>
+        <section>
+            <div>
+                <button>Solo ida</button>
+                <button>Ida y vuelta</button>
+            </div>
+            <div>
+                <input type="search" id="origen" name="origen">
+                <input type="search" id="destino" name="destino">
+            </div>
+            <div>
+                <input type="date"><input type="date">
+            </div>
+            <div>
+                <input type="numeric" >
+            </div>
+        </section>
+    </main>
     
+</body>
+</html>
+    
+
+@endsection
+
+@section('scripts')
 
 @endsection

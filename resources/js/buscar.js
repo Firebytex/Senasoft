88ab1c6471d94c3bbd39c27b4b0ae61357289baf
Andres <vivasandres2007@gmail.com>
2025-10-22 16:04:17 -0500

    Se termina la funcionalidad de la interfaz principal

--- resources/js/buscar.js
+++ resources/js/buscar.js
@@ -1,8 +1,10 @@
-const li = document.getElementsByTagName("li");
+
 const lista1 = document.querySelector("#lista1");
 const lista2 = document.querySelector("#lista2");
 const buscar_origen = document.querySelector("#origen");
 const buscar_destino = document.querySelector("#destino");
+const fecha1 = document.querySelector("#fecha_ida");
+const fecha2 = document.querySelector("#fecha_regreso");
 
 const ida = document.querySelector("#ida");
 const ida_vuelta = document.querySelector("#ida_vuelta");
@@ -10,15 +12,14 @@ const ida_vuelta = document.querySelector("#ida_vuelta");
 function habilitar(){
     const fecha_regreso = document.querySelector("#fecha_regreso");
     fecha_regreso.disabled = true;
-    const fecha = new Date();
-    const fecha_nueva = fecha.getMonth() + 3;
-    console.log(fecha);
-    console.log(fecha_nueva.toString());
+    fecha_regreso.required = false;
+    
 }
 
 function deshabilitar(){
     const fecha_regreso = document.querySelector("#fecha_regreso");
     fecha_regreso.disabled = false;
+    fecha_regreso.required = true;
 }
 
 ida.addEventListener("click", habilitar);
@@ -27,44 +28,52 @@ ida_vuelta.addEventListener("click", deshabilitar);
 
 function barra1(){
     
-    const li = document.getElementsByTagName("li");
-    const origen = document.querySelector("#origen");
+    const li = document.querySelectorAll(".lista1");
 
     lista1.classList.add("hidden");
 
-    origen.addEventListener("keyup",()=>{
-        let texto = origen.value.toLowerCase();
+    buscar_origen.addEventListener("keyup",()=>{
+        let texto = buscar_origen.value.toLowerCase();
         Array.from(li).forEach(e=>{
             let resultados = e.textContent.toLowerCase();
             
             e.classList.add("hidden");
             if(texto !== "" && resultados.startsWith(texto)){
-                console.log(e)
                 lista1.classList.remove("hidden");
                 e.classList.remove("hidden");
+                
+                e.addEventListener("click",()=>{select(e,buscar_origen)});
             }
+
         
         })
 
     })
     }
 
-function barra2(){
-    const li = document.getElementsByTagName("li");
-    const origen = document.querySelector("#destino");
 
+function select(element, barra){
+    let seleccionado = element.textContent;
+    barra.value = seleccionado;
+}
+
+
+function barra2(){
+    const li2 = document.querySelectorAll(".lista2");
     lista2.classList.add("hidden");
 
-    origen.addEventListener("keyup",()=>{
-        let texto = origen.value.toLowerCase();
-        Array.from(li).forEach(e=>{
+    buscar_destino.addEventListener("keyup",()=>{
+        let texto = buscar_destino.value.toLowerCase();
+        console.log("entre en li2")
+        Array.from(li2).forEach(e=>{
+            
             let resultados = e.textContent.toLowerCase();
-
             e.classList.add("hidden");
             if(texto !== "" && resultados.startsWith(texto)){
                 console.log(e)
                 lista2.classList.remove("hidden");
                 e.classList.remove("hidden");
+                e.addEventListener("click",()=>{select(e,buscar_destino)});
             }
         
         })
@@ -73,10 +82,7 @@ function barra2(){
 
 }
 
-barra2()
-    
 
-barra1();
 
 
 
@@ -87,8 +93,59 @@ document.addEventListener("click",()=>{
 
 
 
+function cambiarfechas() {
+    
+
+    const hoy = new Date();
+
+    // Formatear hoy como YYYY-MM-DD
+    const yyyy = hoy.getFullYear();
+    const mm = String(hoy.getMonth() + 1).padStart(2, '0');
+    const dd = String(hoy.getDate()).padStart(2, '0');
+    const hoycadena = `${yyyy}-${mm}-${dd}`;
+
+    fecha1.min = hoycadena;
+    fecha2.min = hoycadena;
+
+
+
+    const maxYYYY = String(hoy.getFullYear());
+    const maxMM = String(hoy.getMonth() + 3);
+    const maxDD = String(hoy.getDate());
+    const maxcadena = `${maxYYYY}-${maxMM}-${maxDD}`;
+
+    fecha1.max = maxcadena;
+    fecha2.max = maxcadena;
+
+    console.log("fecha1.min =", fecha1.min);
+    console.log("fecha1.max =", fecha1.max);
+    console.log("entro en el condicional")
+    
+}
+
 buscar_origen.addEventListener("keyup", ()=>{
     const lista1 = document.querySelector("#lista1");
     const texto = buscar_origen.value.toLowerCase();
     
+    
 })
+function validar(){
+let buscar = document.querySelector("#buscar");
+buscar.addEventListener("click",(e)=>{
+    e.preventDefault();
+    if(fecha2.value< fecha1.value){
+        console.log("entro en el condicional")
+        alert("la fecha de ida no puede ser mayor a la fecha de vuelta, por favor digite nuevamente los campos");
+    }
+
+    if(buscar_origen.value === buscar_destino.value){
+        alert("El destino no puede ser igual al origen, llene nuevamente los campos");
+    }
+})
+}
+
+
+barra2();
+barra1();
+cambiarfechas();
+validar();
--- resources/views/vuelos/index.blade.php
+++ resources/views/vuelos/index.blade.php
@@ -23,31 +23,40 @@
                 <button type="button" class="border border-amber-300" id="ida_vuelta">Ida y vuelta</button>
             </div>
             <div>
-                <input type="search" id="origen" name="origen" placeholder="Origen">
+                <input type="search" id="origen" name="origen" placeholder="Origen" required>
                 <ol id="lista1" class="">
                     @foreach ($lugares as $lugar)
-                     <li value="{{ $lugar->id }}" class="" >{{ $lugar -> nombre }}</li>   
+                     <li value="{{ $lugar->id }}" class="lista1" >{{ $lugar -> nombre }}</li>   
                     @endforeach
                     
                 </ol>
             
-                <input type="search" id="destino" name="destino" placeholder="Destino">
+                <input type="search" id="destino" name="destino" placeholder="Destino" required>
                 <ol id="lista2" class="">
                     @foreach ($lugares as $lugar)
-                    <li value="{{ $lugar->id }}" class="" >{{ $lugar -> nombre }}</li>   
+                    <li value="{{ $lugar->id }}" class="lista2" >{{ $lugar -> nombre }}</li>   
                      @endforeach
                 </ol>
                 
             </div>
             <div>
-                <input type="date" id="fecha_ida" placeholder="Fecha ida" min="2025-10-23" max="2025-12-23">
-                <input type="date" id="fecha_regreso" placeholder="Fecha regreso" min="2025-10-23" max="2025-12-23" disabled>
+                <input type="date" id="fecha_ida" placeholder="Fecha ida" required>
+                <input type="date" id="fecha_regreso" placeholder="Fecha regreso"  disabled>
             </div>
             <div>
-                <input type="numeric">
+                <select required >
+                    <option value="">Seleccione la cantidad de pasajeros</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
             </div>
-            <button >Buscar</button>
+            <button id="buscar" >Buscar</button>
         </form>
+
+        <a href="">Buscar mi reserva</a>
     </main>
  <script src="{{ asset("js/buscar.js") }}"></script>   
 </body>

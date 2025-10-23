66a2c4a0802081ea23d617bf48671559a147d382
Alejandro <alejo40@gmail.com>
2025-10-23 09:26:13 -0500

    pagina de confirmacion

--- resources/views/reservas/confirmacion.blade.php
+++ resources/views/reservas/confirmacion.blade.php
@@ -4,4 +4,23 @@
 
     <h1>Prueba vista</h1>
 
+
+
+    <table>
+        <tr>
+            <th>Vuelo ida origen</th>
+            <th>Vuelo destino</th>
+            <th>Vuelo regreso</th>
+            <th></th>
+            <th></th>
+        </tr>
+
+        <tr>
+            <td>{{ $reserva->vuelo_ida_id }}</td>
+            <td>{{ $reserva->vuelo_regreso_id }}</td>
+            <td>{{ $reserva->pagador_nombre }}</td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
 @endsection
